69f072537a637721d42f09febf9b6bdc2b7f83a8
Karel Muniz <karel.muaizperdomo@nike.com>
2020-03-25 23:22:31 +0100

    Fixed BinaryGap JS

--- codility/java/BinaryGap/solution.js
+++ codility/java/BinaryGap/solution.js
@@ -1,43 +1,29 @@
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
+function solution(N) {
+     // write your code in JavaScript (Node.js 4.0.0)
 
-// NEW recursive function
-function getGaps (BinaryArray, gaps) {
-     // finding the first one via its index
-     const firstOne = BinaryArray.indexOf("1");
-     if (firstOne > -1) {
-          // new array created taking a slice of original array 
-          // from the index of the firstOne + 1 index
-          let NewBinaryArray = BinaryArray.slice(firstOne + 1);
-          // finding second one via its index in new array slice
-          const secondOne = NewBinaryArray.indexOf("1");
-          // accounting for no zeros
-          if (secondOne > 0) {
-               // adding 2 to our gaps array
-               gaps.push(secondOne);
+     var number = N;
+     var binary = '';
+     var counter = -1;
+     var max = 0;
+     while(number > 0) {
+          var digit = number % 2;
+
+          if(digit === 1) {
+               if(counter > max) {
+                    max = counter;
+               }
+               counter = 0;
+          } else if(counter >= 0) {
+               counter++;
           }
- 
-          // Pass array minus second one and gaps array
-          return getGaps(NewBinaryArray.slice(secondOne +1), gaps); 
+
+          binary = '' + digit + binary;
+          number = parseInt(number / 2);
      }
-     
-     // if gaps array length is empty return 0
-     // otherwise return largest value in array
-     return (gaps.length > 0) ? Math.max.apply(Math, gaps) : 0;
-}
-// our function 
-function solution (N) {
-     
-     // Tests if our value is an integer 
-     // Tests if N is within range
-     if (N === parseInt(N, 10) && N >= 1 && N <= 2147483647) {
-          // Convert to binary
-          const Binary = N.toString(2).split('');
-          
-          // calling our recursive function with initial empty gaps
-          return getGaps(Binary, []);
-     }
-      
-     // default if it doesn't meet the requirements
-     return 0;
+
+     //console.log('number, binary:', number, binary);
+
+     return max;
 }
