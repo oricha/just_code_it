75063021a28faaabb995bbef34172224a7374bfe
Karel Muñiz <oricha@gmail.com>
2021-10-25 22:52:18 +0200

    New problems from Algo

---
+++ Algo/DataStructures/BinaryTree.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+public static class BinaryTree {
+    int value;
+    BinaryTree left;
+    BinaryTree right;
+
+    BinaryTree(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
