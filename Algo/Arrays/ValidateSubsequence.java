975911c13a0077c96ee1c068083314a5c27710fe
Karel Muñiz <oricha@gmail.com>
2021-11-01 17:07:10 +0100

    new exe

---
+++ Algo/Arrays/ValidateSubsequence.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+//  Given two non-empty arrays of integers, write a function that determines
+//  whether the second array is a subsequence of the first one.
+//  A subsequence of an array is a set of numbers that aren't necessarily adjacent
+//  in the array but that are in the same order as they appear in the array. For
+//  instance, the numbers
+
+
+class Program {
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+        int arrIdx = 0;
+        int seqIdx = 0;
+        while ( arrIdx < array.size() && seqIdx < sequence.size()) {
+            if(array.get(arrIdx).equals(sequence.get(seqIdx))){
+                seqIdx++;
+            }
+            arrIdx++;
+        }
+        return seqIdx == sequence.size();
+    }
+}
