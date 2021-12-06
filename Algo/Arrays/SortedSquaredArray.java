1f5fd28bb106cdfd5f9e1ca4156271f4e8672435
Karel Muñiz <oricha@gmail.com>
2021-12-06 13:34:36 +0100

    More works Done

--- Algo/Arrays/SortedSquaredArray.java
+++ Algo/Arrays/SortedSquaredArray.java
@@ -2,18 +2,25 @@
 import java.util.*;
 
 
-class SortedSquaredArray {
 
+/*  Write a function that takes in a non-empty array of integers that are sorted
+  in ascending order and returns a new array of the same length with the squares
+  of the original integers also sorted in ascending order.
+
+ */
+
+class SortedSquaredArray {
 
     public int[] sortedSquaredArray(int[] array) {
         // Write your code here.
 
         int[] sortedSquares = new int[array.length];
-        for( int idx=0; ){
-
+        for( int idx=0; idx < array.length; idx++){
+            int value = array[idx];
+            sortedSquares[idx] = value * value;
         }
-
-        return new int[] {};
+        Arrays.sort(sortedSquares);
+        return sortedSquares;
     }
 
 }
---
+++ Algo/BinaryTrees/NodeDepths.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+
+//The distance between a node in a Binary Tree and the tree's root is called the node's depth.
+//Write a function that takes in a Binary Tree and returns the sum of its nodes' depths.
+//Each BinaryTree node has an integer value, a left child node, and a right child node. Children nodes can either be BinaryTree nodes themselves or None / null.
+
+
+class Program {
+
+    public static int nodeDepths(BinaryTree root) {
+        int sumOfDepths = 0;
+        List<Level> stack = new ArrayList<Level>();
+        stack.add(new Level(root, 0));
+        while (stack.size() > 0) {
+            Level top = stack.remove(stack.size() - 1);
+            BinaryTree node = top.root;
+            int depth = top.depth;
+            if (node == null) continue;
+            sumOfDepths += depth;
+            stack.add(new Level(node.left, depth + 1));
+            stack.add(new Level(node.right, depth + 1));
+        }
+        return sumOfDepths;
+    }
+
+    static class Level {
+        public BinaryTree root;
+        int depth;
+
+        public Level(BinaryTree root, int depth) {
+            this.root = root;
+            this.depth = depth;
+        }
+    }
+
+    static class BinaryTree {
+        int value;
+        BinaryTree left;
+        BinaryTree right;
+
+        public BinaryTree(int value) {
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
+}
