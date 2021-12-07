09dbdab21bd61ba1c6d24031951cb831aff7cea7
Karel Muñiz <oricha@gmail.com>
2021-12-07 15:59:43 +0100

    More work from Algo

---
+++ Meta/String/RotationalCipher.java
@@ -0,0 +1,30 @@
+//One simple way to encrypt a string is to "rotate" every alphanumeric character by a certain amount. Rotating a character
+//means replacing it with another character that is a certain number of steps away in normal alphabetic or numerical
+//order.
+//For example, if the string "Zebra-493?" is rotated 3 places, the resulting string is "Cheud-726?". Every alphabetic
+//character is replaced with the character 3 letters higher (wrapping around from Z to A), and every numeric
+//character replaced with the character 3 digits higher (wrapping around from 9 to 0). Note that the non-alphanumeric
+//characters remain unchanged.
+//Given a string and a rotation factor, return an encrypted string.
+//Signature
+//string rotationalCipher(string input, int rotationFactor)
+//Input
+//1 <= |input| <= 1,000,000
+//0 <= rotationFactor <= 1,000,000
+
+
+
+import java.util.*;
+// Add any extra import statements you may need here
+class Main {
+        // Add any helper functions you may need here
+    String rotationalCipher(String input, int rotationFactor) {
+        // Write your code here
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+}
