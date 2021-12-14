9526cc8a6992d9ea852a30b6b3e98184655a86ad
Karel Muñiz <oricha@gmail.com>
2021-12-15 00:20:58 +0100

    my Implementation

--- Codility/java/ING/FIndNTHPrime.java
+++ Codility/java/ING/FIndNTHPrime.java
@@ -7,8 +7,13 @@ import java.util.Scanner;
  or Write a program to find the nth prime number using Java*
  N will be passed as Input of the function*/
 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-class Program {
+public class Solution {
 
     public static int nthPrime(int n) {
         int candidate, count;
@@ -23,8 +28,7 @@ class Program {
     private static boolean isPrime(int n) {
         if (n % 2 == 0) return n == 2;
         if (n % 3 == 0) return n == 3;
-        // 6*k ± 1
-        // 6-
+
         int step = 4, m = (int)Math.sqrt(n) + 1;
         for(int i = 5; i < m; step = 6-step, i += step) {
             if (n % i == 0) {
@@ -32,5 +36,14 @@ class Program {
             }
         }
         return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            System.out.println(nthPrime( n));
+        }
     }
 }
