8e0eb1aa734ebe7dfd3cdb3da5803423d347f906
Karel Muñiz <oricha@gmail.com>
2021-12-14 10:00:38 +0100

    New code

---
+++ Algo/Arrays/ThreeNumberSum.java
@@ -0,0 +1,38 @@
+
+
+//Three Number Sum
+//Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum.
+//The function should find all triplets in the array that sum up to the target sum and return a two-dimensional array
+//of all these triplets. The numbers in each triplet should be ordered in ascending order, and the triplets themselves
+//should be ordered in ascending order with respect to the numbers they hold.
+//
+//If no three numbers sum up to the target sum, the function should return an empty array.
+
+
+import java.util.*;
+
+class Program {
+    // O(n^2) time | O(n) space
+    public static List<Integer[]> threeNumberSum(int[] array, int targetSum) {
+        Arrays.sort(array);
+        List<Integer[]> triplets = new ArrayList<Integer[]>();
+        for (int i = 0; i < array.length - 2; i++) {
+            int left = i + 1;
+            int right = array.length - 1;
+            while (left < right) {
+                int currentSum = array[i] + array[left] + array[right];
+                if (currentSum == targetSum) {
+                    Integer[] newTriplet = {array[i], array[left], array[right]};
+                    triplets.add(newTriplet);
+                    left++;
+                    right--;
+                } else if (currentSum < targetSum) {
+                    left++;
+                } else if (currentSum > targetSum) {
+                    right--;
+                }
+            }
+        }
+        return triplets;
+    }
+}
