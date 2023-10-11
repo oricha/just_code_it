393cd714c8c017026f84064853b17bf016cf58b4
oricha <oricha@gmail.com>
2023-10-11 14:59:41 +0200

    Refactor

--- HackerRank/30_Days_of_Code/Day 1 Data Types/Day1.java
+++ HackerRank/30_Days_of_Code/Day 1 Data Types/Day1.java
@@ -3,38 +3,36 @@ package org.daysofcode.kmuniz.firstweek;
 import java.util.Scanner;
 
 
-
-
 public class Day1 {
 
-	public static void main(String[] args) {
-		int i = 4;
-		double d = 4.0;
-		String s = "HackerRank ";
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
 
-		Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-		/* Declare second integer, double, and String variables. */
-		int integer ;
-		double doubleVar;
-		String str;
+        /* Declare second integer, double, and String variables. */
+        int integer;
+        double doubleVar;
+        String str;
 
 
         /* Read and save an integer, double, and String to your variables.*/
 
-		integer = scan.nextInt();
-		doubleVar = scan.nextDouble();
-		str =scan.next() + scan.nextLine();
+        integer = scan.nextInt();
+        doubleVar = scan.nextDouble();
+        str = scan.next() + scan.nextLine();
         /* Print the sum of both integer variables on a new line. */
-		System.out.println( i + integer);
+        System.out.println(i + integer);
 
         /* Print the sum of the double variables on a new line. */
-		System.out.println( doubleVar + d);
+        System.out.println(doubleVar + d);
 
         /* Concatenate and print the String variables on a new line;
-        	the 's' variable above should be printed first. */
-		System.out.println( s.concat(str));
+            the 's' variable above should be printed first. */
+        System.out.println(s.concat(str));
 
-		scan.close();
-	}
+        scan.close();
+    }
 }
