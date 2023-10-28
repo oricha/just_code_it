67e162ce6210a33af8e5522e8ec4b67144bc9b3b
Karel Muñiz <oricha@gmail.com>
2023-10-28 21:15:32 +0200

    more graphs solution

--- src/algoexpert/Graphs/BSTTraversal.java
+++ src/algoexpert/Graphs/BSTTraversal.java
@@ -1,19 +1,10 @@
 package algoexpert.Graphs;
 
-//Write three functions that take in a Binary Search Tree (BST) and an empty array, traverse the BST, add its nodes'
-//values to the input array, and return that array. The three functions should traverse the BST using the in-order,
-//pre-order, and post-order tree-traversal techniques, respectively.
-//If you're unfamiliar with tree-traversal techniques, we recommend watching the Conceptual Overview section of this
-//question's video explanation before starting to code.
-//Each BST node has an integer value, a left child node, and a right child node. A node is said to be a valid BST
-//node if and only if it satisfies the BST property: its value is strictly greater than the values of every node to
-//its left; its value is less than or equal to the values of every node to its right; and its children nodes are either
-//valid BST nodes themselves or None / null.
 
 
 import java.util.*;
 
-class Program {
+class BSTTraversal {
     // O(n) time | O(n) space
     public static List<Integer> inOrderTraverse(BST tree, List<Integer> array) {
         if (tree.left != null) {
---
+++ src/algoexpert/Graphs/BreadthFirstSearch.java
@@ -0,0 +1,50 @@
+package algoexpert.Graphs;
+
+/**
+ * Depth-first Search
+ * You're given a Node class that has a name and an array of optional children nodes. When put together,
+ * nodes form an acyclic tree-like structure.
+ * Implement the depthFirstSearch method on the Node class, which takes in an empty array, traverses the tree
+ * using the Depth-first Search approach (specifically navigating the tree from left to right), stores all of the
+ * nodes' names in the input array, and returns it.
+ * If you're unfamiliar with Depth-first Search, we recommend watching the Conceptual Overview section of
+ * this question's video explanation before starting to code.
+
+ * Sample Input
+ * graph = A
+ *      /  |  \
+ *     B   C   D
+ *    / \     / \
+ *   E   F   G   H
+ *      / \   \
+ *     I   J   K
+ * Sample Output
+ * ["A", "B", "E", "F", "I", "J", "C", "D", "G", "K", "H"]
+ */
+
+import java.util.*;
+public class BreadthFirstSearch {
+    static class Node {
+        String name;
+        List<Node> children = new ArrayList<Node>();
+        public Node(String name) {
+            this.name = name;
+        }
+        // O(v + e) time | O(v) space
+        public List<String> breadthFirstSearch(List<String> array) {
+            Queue<Node> queue = new LinkedList<Node>();
+            queue.add(this);
+            while (!queue.isEmpty()) {
+                Node current = queue.poll();
+                array.add(current.name);
+                queue.addAll(current.children);
+            }
+            return array;
+        }
+        public Node addChild(String name) {
+            Node child = new Node(name);
+            children.add(child);
+            return this;
+        }
+    }
+}
